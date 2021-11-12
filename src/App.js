5b0f6c4ca0588afbd8a6c7c8737e3a901693866c
Rahima Salmanova <rehimesalman7@gmail.com>
2021-11-12 13:19:45 +0400

    add folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            <code>OAS</code>
+          </h1>
+          <h5>Office Administration System</h5>
+        </header>
+      </div>
   );
 }
 
