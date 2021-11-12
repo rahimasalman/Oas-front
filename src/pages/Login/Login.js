1662a93373ca1630580c026fa47c02943dc8fb51
NarminGale <narmingale@gmail.com>
2021-11-12 15:45:43 +0400

    Setup files

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,21 +1,7 @@
-import React, { useState } from "react";
+import React from 'react'
 
-
-export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
-
-    return (
-        <div className="Login">
-
-        </div>
-    );
+function Login() {
+  return <div></div>
 }
+
+export default Login
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SignUp() {
+  return <div className="container"></div>
+}
+
+export default SignUp
