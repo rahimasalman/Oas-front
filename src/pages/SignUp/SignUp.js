5cb3f3c61fc5b28f21a471cafd6544e77f680663
NarminGale <narmingale@gmail.com>
2021-11-15 14:12:00 +0400

    Modified codes

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -2,23 +2,22 @@ import React from 'react'
 import image from '../../images/office-2.jpeg'
 import Button from '../../components/Button/Button'
 import Form from '../../components/Form/Form'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 function SignUp() {
   return (
     <div className="main row">
-      <div className="left-side col-lg-6 col-sm-12">
-        <img src="" />
+      <div className="left-side col-md-6 col-sm-12">
         <img src={image} alt="background" className="back-image" />
       </div>
-      <div className="right-side col-lg-6 col-sm-12 d-flex flex-column justify-content-center px-5">
+      <div className="right-side col-md-6 col-sm-12 d-flex flex-column justify-content-center px-5">
         <h1>
           Welcome to
           <span className="d-block"> Office Administration System</span>
         </h1>
         <p>We make job life easy for everone</p>
         <form className="my-3">
-<Form/>
+          <Form />
           <div class="form-group mb-3">
             <label for="exampleInputPassword1">Confirm password</label>
             <input
@@ -28,12 +27,12 @@ function SignUp() {
               placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
             />
           </div>
-<Button
-text="Sign Up"/>
+          <Button text="Sign Up" />
           <p>
             Already have an account?
-              <Link to="/signin" className="m-2">Sign In</Link>
-
+            <Link to="/signin" className="m-2">
+              Sign In
+            </Link>
           </p>
         </form>
       </div>
