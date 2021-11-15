42e259e981d94ab12e2621f99cf206da5f968662
NarminGale <narmingale@gmail.com>
2021-11-15 15:15:37 +0400

    Added eye icon for confirm password

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import { React, useState } from 'react'
 import image from '../../images/office-2.jpeg'
 import Button from '../../components/Button/Button'
 import Form from '../../components/Form/Form'
 import { Link } from 'react-router-dom'
 
 function SignUp() {
+  const [confirmPass, setConfirmPass] = useState('password')
+  const [confirmEye, setConfirmEye] = useState('fa fa-eye-slash')
+
+  const onClickEye = () => {
+    if (confirmPass === 'password') {
+      setConfirmPass('text')
+      setConfirmEye('fa fa-eye')
+    } else {
+      setConfirmPass('password')
+      setConfirmEye('fa fa-eye-slash')
+    }
+  }
   return (
     <div className="main row">
       <div className="left-side col-md-6 col-sm-12">
@@ -21,11 +33,14 @@ function SignUp() {
           <div class="form-group mb-3">
             <label for="exampleInputPassword1">Confirm password</label>
             <input
-              type="password"
+              type={confirmPass}
               class="form-control"
               id="exampleInputPassword1"
               placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
             />
+            <span className="togglePassword" onClick={onClickEye}>
+              <i className={confirmEye}></i>
+            </span>
           </div>
           <Button text="Sign Up" />
           <p>
