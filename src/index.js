f04369c4d228b57ad185c890627ad2096cbe1e28
NarminGale <narmingale@gmail.com>
2021-11-15 15:16:02 +0400

    Imported font-awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './index.scss'
 
 ReactDOM.render(
