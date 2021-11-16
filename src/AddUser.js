346e671dd0b8295fcc68e141054ce549b8b151ca
NarminGale <narmingale@gmail.com>
2021-11-16 17:01:29 +0400

    Created functionality of deleting

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,56 +1,53 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom'
-import {
-    Form,
-    FormGroup,
-    Label,
-    Input,
-    Button
-} from 'reactstrap'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
-const AddUser = () =>{
-    const [user, setUser] = useState({
-        username: "",
-        email: ""
-    });
-    const {username, email} = user
-    const onInputChange= (e) => {
-        setUser({...user, [e.target.name]: e.target.value})
-    };
+const AddUser = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+  })
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-    }
+  const { username, email } = user
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
 
-    return (
-        <>
-            <h2 className="text-center mb-4">Add a user</h2>
-            <Form className="container m-3" onSubmit={e=> onSubmit(e)}>
-                <FormGroup>
-                    <Label>Name</Label>
-                    <Input type='text'
-                           name="username"
-                           placeholder="Enter Name"
-                    value={username}
-                           onChange={e => onInputChange(e)}
-                    ></Input>
-                </FormGroup>
-                <FormGroup>
-                    <Label>Email</Label>
-                    <Input type='email'
-                           placeholder="Enter email"
-                    value={email}
-                           name="email"
-                    onChange={e => onInputChange(e)}></Input>
-                </FormGroup>
-                <Button type='submit' color="success">
-                    Submit
-                </Button>
-                <Link to="/home" className="btn btn-danger m-2">Cancel</Link>
-            </Form>
-        </>
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
 
-    );
+  return (
+    <>
+      <h2 className="text-center mb-4">Add a user</h2>
+      <Form className="container m-3" onSubmit={(e) => onSubmit(e)}>
+        <FormGroup>
+          <Label>Name</Label>
+          <Input
+            type="text"
+            name="username"
+            placeholder="Enter Name"
+            value={username}
+            onChange={(e) => onInputChange(e)}></Input>
+        </FormGroup>
+        <FormGroup>
+          <Label>Email</Label>
+          <Input
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            name="email"
+            onChange={(e) => onInputChange(e)}></Input>
+        </FormGroup>
+        <Button type="submit" color="success">
+          Submit
+        </Button>
+        <Link to="/home" className="btn btn-danger m-2">
+          Cancel
+        </Link>
+      </Form>
+    </>
+  )
 }
 
-export default AddUser;
+export default AddUser
--- src/data.js
+++ src/data.js
@@ -1,33 +1,34 @@
-export const employees = [
+const data = [
   {
     ID: 1,
-    Username: "Harry Potter"
+    Username: 'Harry Potter',
     Email: 'example@vabiss.com',
-
   },
   {
     ID: 2,
-    Username: "Harry Potter"
-    Email: 'example@vabiss.com
+    Username: 'Harry Potter',
+    Email: 'example@vabiss.com',
   },
   {
     ID: 3,
-    Username: "Harry Potter"
-    Email: 'example@vabiss.com
+    Username: 'Harry Potter',
+    Email: 'example@vabiss.com',
   },
   {
     ID: 4,
-    Username: "Harry Potter"
-    Email: 'example@vabiss.com
+    Username: 'Harry Potter',
+    Email: 'example@vabiss.com',
   },
   {
     ID: 5,
-    Username: "Harry Potter"
-    Email: 'example@vabiss.com
+    Username: 'Harry Potter',
+    Email: 'example@vabiss.com',
   },
   {
     ID: 6,
-    Username: "Harry Potter"
-    Email: 'example@vabiss.com
+    Username: 'Harry Potter',
+    Email: 'example@vabiss.com',
   },
 ]
+
+export default data
