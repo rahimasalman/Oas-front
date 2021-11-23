8865d97b7a000e6a81a9151633437b266ea41abc
NarminGale <narmingale@gmail.com>
2021-11-23 12:02:16 +0400

    Completed add and delete button functionality

--- front/src/pages/Categories/Categories.js
+++ front/src/pages/Categories/Categories.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link, useNavigate, Navigate } from 'react-router-dom'
+
 import Sidenav from '../../components/Sidenav'
 import iconEdit from '../../images/icon-edit.svg'
 import iconDelete from '../../images/icon-delete.svg'
@@ -7,51 +10,110 @@ import { FaPlus } from 'react-icons/fa'
 
 function Categories() {
   const [category, setCategory] = useState({
-    category:''
+    name: '',
   })
+  const [editedCategory, setEditedCategory] = useState('')
   const [list, setList] = useState([])
   const [isEditing, setIsEditing] = useState(false)
   const [editId, setEditId] = useState(null)
+  const [deleteId, setDeleteId] = useState(null)
 
-  const handleSubmit = (e) => {
+  const tokenUsers = localStorage.getItem('tokenUsers')
+
+  const onInputChange = (e) => {
+    setCategory({ ...category, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
+
+    await axios
+      .post(
+        `${process.env.REACT_APP_API_URL}api/categories`,
+        {
+          name: category.name,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer 46|s2qMomccCBR0uO1dw6v1W9qSwtIFd6xaTXxh3FTe',
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res)
+        setCategory({
+          name: '',
+        })
+        getCategories()
+      })
+      .catch((err) => console.log(err))
+
     if (!category) {
       // display alert
-    } else if (category && isEditing) {
+    } else if (editedCategory && isEditing) {
       // editing
       setList(
         list.map((item) => {
           if (item.id === editId) {
-            return { ...item, title: category }
+            return { ...item, title: editedCategory }
           }
           return item
         })
       )
-      setCategory('')
+      setEditedCategory('')
       setEditId(null)
       setIsEditing(false)
     } else {
-      // show alert
-      const newCategory = {
-        id: new Date().getTime().toString(),
-        title: category,
-      }
-      list.push(newCategory)
-      setCategory('')
-      console.log(list)
+      // show <alert>                                                                                                                                                                                                                                                                                     </alert>
     }
   }
 
+  const navigate = useNavigate()
+
+  const getCategories = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}api/categories`, {
+        headers: {
+          Authorization: 'Bearer 46|s2qMomccCBR0uO1dw6v1W9qSwtIFd6xaTXxh3FTe',
+        },
+      })
+      .then((res) => {
+        if (res.data.categories) {
+          console.log(res.data.categories)
+          setList(res.data.categories)
+        } else {
+          setList([])
+        }
+      })
+      .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    tokenUsers ? getCategories() : navigate('/')
+  }, [tokenUsers, navigate])
+
   const removeItem = (id) => {
-    setList(list.filter((item) => item.id !== id))
+    // setList(list.filter((item) => item.id !== id))
+    console.log(id)
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}api/categories/${id}`, {
+        headers: {
+          Authorization: 'Bearer 46|s2qMomccCBR0uO1dw6v1W9qSwtIFd6xaTXxh3FTe',
+        },
+      })
+      .then((res) => {
+        getCategories()
+      })
+      .catch((err) => console.log(err))
   }
 
   const editItem = (id) => {
     const specificItem = list.find((item) => item.id === id)
     setIsEditing(true)
     setEditId(id)
-    setCategory(specificItem.title)
-    console.log(specificItem)
+    setEditedCategory(specificItem.title)
+    console.log(specificItem.title)
   }
 
   return (
@@ -100,7 +162,7 @@ function Categories() {
                   </button>
                 </div>
                 <div className="modal-body">
-                  <form onSubmit={handleSubmit}>
+                  <form onSubmit={(e) => handleSubmit(e)}>
                     <div className="mb-3">
                       <label
                         htmlFor="validationCustomUsername"
@@ -115,22 +177,24 @@ function Categories() {
                           aria-describedby="inputGroupPrepend"
                           placeholder="Enter"
                           required
-                          value={category}
-                          onChange={(e) => {
-                            setCategory(e.target.value)
-                          }}
+                          name="name"
+                          value={category.name}
+                          onChange={(e) => onInputChange(e)}
                         />
                         <div className="invalid-feedback">
                           Please choose a username.
                         </div>
                       </div>
                     </div>
-                    <button type="submit" className="btn btn-success mr-2">
+                    <button
+                      type="submit"
+                      className="btn btn-success"
+                      data-bs-dismiss="modal">
                       Save
                     </button>
                     <button
-                      type="button"
-                      className="btn btn-light"
+                      type="submit"
+                      className="btn btn-light mx-3"
                       data-bs-dismiss="modal">
                       Cancel
                     </button>
@@ -160,14 +224,75 @@ function Categories() {
               return (
                 <tr key={index}>
                   <th scope="row">{item.id}</th>
-                  <td>{item.title}</td>
                   <td>
-                    <button onClick={() => editItem(item.id)}>
+                    {isEditing ? (
+                      <input
+                        disabled
+                        value={editedCategory}
+                        onChange={(e) => setEditedCategory(e.target.value)}
+                      />
+                    ) : (
+                      <span>{item.name}</span>
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      data-bs-toggle="modal"
+                      onClick={() => editItem(item.id)}>
                       <img src={iconEdit} />
                     </button>
-                    <button onClick={() => removeItem(item.id)}>
+                    <button
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal1"
+                      onClick={() => setDeleteId(item.id)}>
                       <img src={iconDelete} />
                     </button>
+                    <div
+                      className="modal fade"
+                      id="exampleModal1"
+                      tabIndex="-1"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true">
+                      <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">
+                              Confirm Deletion
+                            </h5>
+                            <button
+                              type="button"
+                              className="close-button"
+                              data-bs-dismiss="modal"
+                              aria-label="Close">
+                              <img src={iconClose} />
+                            </button>
+                          </div>
+                          <div className="modal-body">
+                            <form>
+                              <div className="my-3 text-center">
+                                <p className="fw-bold">Delete this row?</p>
+                                <span>This action can’t be undone</span>
+                              </div>
+                              <div className="d-flex justify-content-center">
+                                <button
+                                  type="button"
+                                  data-bs-dismiss="modal"
+                                  className="btn btn-danger mx-3"
+                                  onClick={() => removeItem(deleteId)}>
+                                  Delete
+                                </button>
+                                <button
+                                  type="button"
+                                  className="btn btn-light mx-3"
+                                  data-bs-dismiss="modal">
+                                  Cancel
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </td>
                 </tr>
               )
